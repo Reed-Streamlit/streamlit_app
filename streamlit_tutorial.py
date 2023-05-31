889d2cd7a559a9727334b0e983739285416636e6
keithcheah <99089668+keithcheah@users.noreply.github.com>
2023-05-31 17:53:54 +0100

    Add files via upload

--- streamlit_tutorial.py
+++ streamlit_tutorial.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[4]:
 
 
 import streamlit as st
 
 # Function to predict output based on user inputs
-def predict_output(a, b, c, d, e):
+def predict_output(pirads, age, psa, vol, adeno, grade, white, black, asian, hispanic, other):
     # Placeholder implementation
     # Replace this with your trained model's prediction logic
-    return a + b + c + d + e
+    return pirads + age + psa + vol + adeno + grade + white + black + asian + hispanic + other
 
 def main():
     # Set page title
     st.title("Numerical Output Prediction")
 
     # Add input fields for user to enter feature values
-    a = st.number_input("Enter value for feature A", value=0.0)
-    b = st.number_input("Enter value for feature B", value=0.0)
-    c = st.number_input("Enter value for feature C", value=0.0)
-    d = st.number_input("Enter value for feature D", value=0.0)
-    e = st.number_input("Enter value for feature E", value=0.0)
-
+    pirads = st.selectbox("PIRADS score", options=[1, 2, 3, 4, 5])
+    age = st.number_input("Age", min_value=0, max_value=120, value=60)
+    psa = st.number_input("PSA (ng/mL)", min_value=0.0, value=1.0)
+    vol = st.number_input("Prostate volume (cm^3)", min_value=10.0, max_value=200.0, value=10.0)
+    adeno = st.radio("Adenopathy? (0=No/1=Yes)", options=[0, 1])
+    grade = st.selectbox("Biopsy grade", options=[1, 2, 3, 4, 5])
+    
+    race = st.radio("Race", ("White", "Black", "Asian", "Hispanic", "Other"), horizontal = True)
+    white = (race == "White") * 1
+    black = (race == "Black") * 1
+    asian = (race == "Asian") * 1
+    hispanic = (race == "Hispanic") * 1
+    other = (race == "Other") * 1
+    
     # Predict the output based on user inputs
-    output = predict_output(a, b, c, d, e)
+    output = predict_output(pirads, age, psa, vol, adeno, grade, white, black, asian, hispanic, other)
 
     # Display the predicted output
     st.write("Predicted Output:", output)
