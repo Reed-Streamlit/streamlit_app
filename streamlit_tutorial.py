4fa401b3cdac96628153a5aadd2ab228a3d2beb7
keithcheah <99089668+keithcheah@users.noreply.github.com>
2023-05-31 17:26:04 +0100

    Add files via upload

--- streamlit_tutorial.py
+++ streamlit_tutorial.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[3]:
 
 
 import streamlit as st
-st.write("Hello")
+
+# Function to predict output based on user inputs
+def predict_output(a, b, c, d, e):
+    # Placeholder implementation
+    # Replace this with your trained model's prediction logic
+    return a + b + c + d + e
+
+def main():
+    # Set page title
+    st.title("Numerical Output Prediction")
+
+    # Add input fields for user to enter feature values
+    a = st.number_input("Enter value for feature A", value=0.0)
+    b = st.number_input("Enter value for feature B", value=0.0)
+    c = st.number_input("Enter value for feature C", value=0.0)
+    d = st.number_input("Enter value for feature D", value=0.0)
+    e = st.number_input("Enter value for feature E", value=0.0)
+
+    # Predict the output based on user inputs
+    output = predict_output(a, b, c, d, e)
+
+    # Display the predicted output
+    st.write("Predicted Output:", output)
+
+if __name__ == "__main__":
+    main()
 
