1a4562050b357b3f02c5ac2db7a8ddce9f22699e
Reed-Streamlit <143527623+Reed-Streamlit@users.noreply.github.com>
2023-09-14 10:28:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,22 +24,15 @@ def predict_output(input_df):
     #be malware
     #see more here: https://docs.python.org/3/library/pickle.html
     LNM_model = pickle.load(open('rnd_clf_opt_rndcv.sav', 'rb'))
-    # got error: ReadstatError: Invalid file, or file has unsupported features
-    # LNM_model = pd.read_spss('rnd_clf_opt_rndcv.sav')
 
-    # OUTPUT = predicted probability of LNM
     output = LNM_model.predict(input_df)
     pred = float(LNM_model.predict(input_df))
     return pred
 
-def setup_inputs():
-
-
-def main():
+def create_inputs():
     # Set page title
     st.title("Numerical Output Prediction")
 
-    # Add input fields for user to enter feature values
     pirads = st.selectbox("PIRADS score", options=[1, 2, 3, 4, 5])
     age = st.number_input("Age", min_value=0, max_value=120, value=60)
     psa = st.number_input("PSA (ng/mL)", min_value=0.0, value=1.0)
@@ -57,17 +50,16 @@ def main():
     input_dict = {"P_Score": pirads, "age_rp": age, "PSA": psa,
                  "prostate_volume": vol, "adenopathy": adeno, 
                  "grade_prostate_bx": grade, "white_race": white}
-
-    input_df = pd.DataFrame([input_dict])
     
+    return input_dict
+
+def main():
+    input_dict = create_inputs()
+    input_df = pd.DataFrame([input_dict])
     center_button = st.button("Estimate")
     
     if center_button:
-
-        # Predict the output based on user inputs
         output = predict_output(input_df)
-   
-        # Display the predicted output
         st.write("Lymph node metastasis?", output)
 
 if __name__ == "__main__":
