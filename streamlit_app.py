6bdc668245fc8eadc5c0f8b7269b88a429c07583
Reed-Streamlit <143527623+Reed-Streamlit@users.noreply.github.com>
2023-09-14 16:08:29 -0700

    Streamline streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,30 +9,14 @@ import pandas as pd
 import numpy as np
 import sklearn
 
-#unpickling model
-model = pickle.load(open('rnd_clf_opt_rndcv.sav', 'rb'))
+########## Change ##########
+MODEL_NAME = 'rnd_clf_opt_rndcv.sav'
+OUTPUT_MSG = 'Lymph node metastasis?'
+############################
 
-def format_model_input(df):
-    #return df.values
-    return df
-
-def format_model_output(pred):
-    return pred[0]
-    
-def predict_output(input_df: pd.DataFrame) -> float:
+def create_user_inputs() -> pd.DataFrame:
     """
-    Args:
-        a dataframe containing values from the inputs selected by the user
-    Returns:
-        output of model prediction as determined by the format_model_output function
-    """
-    formatted_input = format_model_input(input_df)
-    pred = model.predict(formatted_input)
-    return format_model_output(pred)
-
-def create_user_inputs():
-    """
-    Generates and formats all inputs user sees on page, outputs the values to be passed to model for prediction.
+    Generates and formats all inputs user sees on page, outputs the values as a dataframe to be passed to model for prediction.
     
     Returns:
         a dataframe containing values from the inputs selected by the user
@@ -43,6 +27,7 @@ def create_user_inputs():
     pirads = st.selectbox("PIRADS score", options=[1, 2, 3, 4, 5])
     age = st.number_input("Age", min_value=0, max_value=120, value=60)
     psa = st.number_input("PSA (ng/mL)", min_value=0.0, value=1.0)
+    
     vol = st.number_input("Prostate volume (cm^3)", min_value=10.0, max_value=200.0, value=10.0)
     adeno = st.radio("Adenopathy? (0=No/1=Yes)", options=[0, 1])
     grade = st.selectbox("Biopsy grade", options=[1, 2, 3, 4, 5])
@@ -54,15 +39,15 @@ def create_user_inputs():
     
     return pd.DataFrame([input_dict])
 
-
-
 def main():
+    #unpickling model
+    model = pickle.load(open(MODEL_NAME, 'rb'))
     user_input_df = create_user_inputs()
     center_button = st.button("Estimate")
     
-    if center_button:
-        output = predict_output(user_input_df)
-        st.write("Lymph node metastasis?", output)
+    if center_button: #if button is clicked
+        output = model.predict(input_df)
+        st.write(OUTPUT_MSG, output)
 
 if __name__ == "__main__":
     main()
